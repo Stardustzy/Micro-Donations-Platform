06b4550d29ac767d9757f2e3a941a53309e246b7
benjaminmweribaya <b3njaminbaya@gmail.com>
2025-01-29 11:48:54 +0300

    Setup main App component with React Router routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import AppRoutes from './routes/AppRoutes';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
-const App = () => <AppRoutes />;{
+function App() {
   return (
-    <div className="text-center">
-      <h1 className="text-4xl font-bold text-blue-500">Welcome to the Micro-Donation Platform</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* More routes will be added here later */}
+        </Routes>
+      </div>
+    </>
   );
-};
+}
 
 export default App;
+
