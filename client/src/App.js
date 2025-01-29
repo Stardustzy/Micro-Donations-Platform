99b693786b3d3a9e44b97f399ab4b77be80bcd7b
benjaminmweribaya <b3njaminbaya@gmail.com>
2025-01-29 11:58:51 +0300

    Add routes for Login and Register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <div className="container mt-4">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           {/* More routes will be added here later */}
         </Routes>
       </div>
