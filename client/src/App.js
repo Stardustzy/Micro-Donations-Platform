a37f50f8bf0809cca0a7367e8d9d92cc5d482e98
benjaminmweribaya <b3njaminbaya@gmail.com>
2025-01-29 13:04:13 +0300

    Secure private routes using ProtectedRoute

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard"; // Private page
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <div className="container mt-4">
+    <AuthProvider>
+      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
           {/* More routes will be added here later */}
         </Routes>
-      </div>
-    </>
+      </Router>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
 
