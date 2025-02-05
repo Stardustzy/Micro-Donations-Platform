0e248d5e6795f0dd581a43422437370a1d8b9478
benjaminmweribaya <b3njaminbaya@gmail.com>
2025-02-06 01:14:52 +0300

    debug

--- client/src/pages/Causes.js
+++ client/src/pages/Causes.js
@@ -177,19 +177,6 @@ const Causes = () => {
                 </div>
             )}
 
-            {/* Pagination */}
-            <nav aria-label="Page navigation">
-                <ul className="pagination justify-content-center">
-                    {Array.from({ length: Math.ceil(filteredCauses.length / itemsPerPage) }, (_, index) => (
-                        <li key={index} className={`page-item ${currentPage === index + 1 ? "active" : ""}`}>
-                            <button className="page-link" onClick={() => handlePageChange(index + 1)}>
-                                {index + 1}
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-
             {/* CTA Section */}
             <section className="my-4">
                 <div className="card text-center p-4 shadow-lg border-0">
